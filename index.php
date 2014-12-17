8c6a75c75a8cc9ea0320b90272177a2deae4fd32
Kyusuf Muhammad <kyusufm@gmail.com>
2014-12-17 11:20:52 +0700

    day 2 learn sass to make theme

--- index.php
+++ index.php
@@ -7,44 +7,69 @@
 </head>
 <body>
 <div class="container">
-	<div class="header">
-		<div class="nav">
-			<div class="nav-logo"></div>
-			<ul class="nav-menu">
-				<li>
-					<div class="nav"> Menu 1</div>
-				</li>
-				<li>
-					<div class="nav"> Menu 2</div>
-				</li>
-			</ul>
+	<header class="header">
+		<div class="navbar">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+			</div>
+            <a class="navbar-brand" href="#"><img src="images/logo.png" alt="logo"></a>
+			<div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="">Blog Single</a></li>
+                                <li><a href="">Pricing</a></li>
+                                <li><a href="">404</a></li>
+                                <li><a href="">Shortcodes</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">Blog</a></li> 
+                        <li><a href="">Contact</a></li>                        
+                    </ul>
+                </div>			
 		</div>
 		<div class="introduction">
 			<div class="image-prev"></div>
 			<div class="front-quote"></div>
 		</div>
-	</div>
-	<div class="row content">
-		<div class="content-container col-md-4">
-			<div class="content-title"> Judul Konten </div>
+	</header>
+	<div class="row">
+		<div class="content-container col-md-4 col-sm-4">
+			<div class="content">
+			<div class="content-title h3"> Judul Konten </div>
 			<div class="content-ss"> image </div>
-			<div class="content-prev"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla repellat quae ducimus optio sequi suscipit mollitia...</div>
+			<p class="content-prev"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla repellat quae ducimus optio sequi suscipit mollitia...</p>
 			<div class="content-detail">2014 by Author | (20) Comments</div>
 			<div class="content-social">| t | f | g+ |</div>
+			</div>
 		</div>
-		<div class="content-container col-md-4">
-			<div class="content-title"> Judul Konten </div>
+		<div class="content-container col-md-4 col-sm-4">
+		<div class="content">
+			<div class="content-title h3"> Judul Konten </div>
 			<div class="content-ss"> image </div>
 			<div class="content-prev"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla repellat quae ducimus optio sequi suscipit mollitia...</div>
 			<div class="content-detail">2014 by Author | (20) Comments</div>
 			<div class="content-social">| t | f | g+ |</div>
+			</div>
 		</div>
-		<div class="content-container col-md-4">
-			<div class="content-title"> Judul Konten </div>
+		<div class="content-container col-md-4 col-sm-4">
+		<div class="content">
+			<div class="content-title h3"> Judul Konten </div>
 			<div class="content-ss"> image </div>
 			<div class="content-prev"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla repellat quae ducimus optio sequi suscipit mollitia...</div>
 			<div class="content-detail">2014 by Author | (20) Comments</div>
 			<div class="content-social">| t | f | g+ |</div>
+			</div>
 		</div>
 	</div>
 </div>
